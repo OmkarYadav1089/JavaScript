8838d47ec786808eb5a415fc59a437d0f3e46316
Omkar Yadav <154461228+OmkarYadav1089@users.noreply.github.com>
2024-11-14 16:16:50 +0530

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+// 'use strict' treat all users as newer version
+
+// alert(3+3) we are sing node.js not browser
+
+// u must make ur code easily readable and understandable
+
+// numbers - all numbers from 2 to 2 power53
+
+// bigint - all bigger intergesrs used in trading  
+
+// boolean- true/false wither true or false
+
+// string - all the collection of characters mostly all data in single and double quotes 
+
+// null: it is the standalone value its type is object
+//undefined : it is the undefined data type
+
+//symbol : uniqueness
+
+//object
